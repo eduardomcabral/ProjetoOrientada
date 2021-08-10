339857762ca3a7be751c5998245d30edae2bdcc0
eduardocabral1 <eduardocabral@live.com>
2021-08-09 21:05:05 -0300

    Versao 2.8

--- src/Interface/AtualizarInfo.java
+++ src/Interface/AtualizarInfo.java
@@ -1,9 +1,13 @@
 package Interface;
+import Paciente.Paciente;
+import Medico.Medico;
 
 public interface AtualizarInfo{
 	
-	public String atualizarInfo();
-	
-	
+	public Paciente atualizarInfo(Paciente paciente, String nome, int idade, String sexo, String sintoma) {
+		pessoa.setNome(nome);
+		pessoa = this.setPessoa(paciente, idade, sexo, sintoma);
+		return pessoa;
+	}
 	
 }
